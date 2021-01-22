f8c2ce596876114f826dcdd40ce661a4fc696d7b
BlopGuy <gvieira.assuntos@outlook.pt>
2021-01-22 09:43:07 +0000

    commit ot chnage computers

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
+let canvas = document.querySelector('#canvas');
+let context = canvas.getContext('2d');
+
+
+
+
+
+
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    let background = new Image();
+    background.src = './images/road.png';
+
+    background.onload = function(){
+      context.drawImage(background, 0, 0);
+    };
+
+    
+  }
 };
